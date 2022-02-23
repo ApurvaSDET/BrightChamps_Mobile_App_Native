039453c6f8c1a3362db3645ba080a979a42a9b15
apurvakushwaha <ishu.kush@gmail.com>
2022-02-23 18:14:29 +0530

    Pipeline Setup

--- src/test/java/Trial/Test.java
+++ src/test/java/Trial/Test.java
@@ -1,36 +1,9 @@
 package Trial;
 
-import com.ssts.pcloudy.Connector;
-import com.ssts.pcloudy.dto.access.UserDetailResult;
-import com.ssts.pcloudy.dto.device.MobileDevice;
-import com.ssts.pcloudy.dto.file.PDriveFileDTO;
-import com.ssts.pcloudy.dto.generic.BookingDtoResult;
-//import com.ssts.pcloudy.exception.ConnectError;
-import org.apache.commons.lang.StringUtils;
-import java.io.*;
-import java.util.List;
-import java.util.Scanner;
 
 public class Test {
 
     public static void main(String[] args) {
-
-
-        //Connector con = new Connector("https://device.pcloudy.com");
-        // authToken = con.authenticateUser("ishu.kush@gmail.com", "rmt4p5cjj8g56wzn9gfys3kj");
-
-        //PDriveFileDTO uploadedApp = con.uploadApp(authToken, new File("/Users//apurvakushwaha/Desktop/app-arm64-v8a-release.apk"));
-
-        //BookingDtoResult bookedDevice = con.bookDevice(authToken, 10, selectedDevice);
-
-
-        //con.deleteFileFromCloud(authToken, "app-arm64-v8a-release.apk", "data");
-
-        //MobileDevice selectedDevice = con.chooseSingleDevice(authToken, "Android");
-
-
-        //List<MobileDevice> selectedDevices = con.chooseMultipleDevices(authToken, "Android");
-
 
     }
 
