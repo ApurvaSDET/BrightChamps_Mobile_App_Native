337dc5a647be883a505ec7f1e8c83d96e360f3d6
apurvakushwaha <ishu.kush@gmail.com>
2022-04-18 19:58:04 +0530

    Bug Fix

--- src/test/java/StepDefinition/Upgrade_Package.java
+++ src/test/java/StepDefinition/Upgrade_Package.java
@@ -22,7 +22,6 @@ public class Upgrade_Package extends BaseUtil {
 
         //waiting till landing on login screen
         _wait(PO.LoginWithPassword);
-        Thread.sleep(1000);
 
         //*******************Logic to land on Master login Page*************************
 
@@ -30,16 +29,17 @@ public class Upgrade_Package extends BaseUtil {
         MobileElement Hidden_menu = (MobileElement) driver.findElement(PO.Hidden_Menu);
 
         int x = Hidden_menu.getLocation().getX();
-        Thread.sleep(1000);
         int y = Hidden_menu.getLocation().getY();
 
         //Scrolling randomly Day section
         TouchAction touchAction = new TouchAction(driver);
-        for(int i=0; i<=7; i++)
-            Thread.sleep(350);
+        for(int i=0; i<=7; i++) {
+            System.out.println(_get_current_time_in_sec());
             touchAction.tap(PointOption.point(x + 15, y + 20)).perform();
+        }
 
         //Asserting the Student Home Page
+        _wait(PO.Submit);
         Assert.assertTrue(_is_displayed(PO.Submit));
 
     }
